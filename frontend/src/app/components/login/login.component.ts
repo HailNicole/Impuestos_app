71f6c7906d410d97f1e4a90ee8567a7561fddfa2
HailNicole <mconstantem1@est.ups.edu.ec>
2024-07-04 20:10:42 -0500

    N

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -11,30 +11,15 @@ import {User} from '../../models/user';
 })
 export class LoginComponent {
   formulario: FormGroup;
-  /*readonly email = new FormControl('', [Validators.required, Validators.email]);
-  readonly password = new FormControl('', [Validators.required, Validators.nullValidator]);*/
   errorMessage = signal('');
   email:string = "";
   password:string = "";
 
   constructor(private authService:AuthService, private router:Router, private fb: FormBuilder){
-    /*merge(this.email.statusChanges, this.email.valueChanges)
-      .pipe(takeUntilDestroyed())
-      .subscribe(() => this.updateErrorMessage());*/
     this.formulario = this.fb.group({email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.nullValidator]]
     });
   }
-
-  /*updateErrorMessage() {
-    if (this.email.hasError('required')) {
-      this.errorMessage.set('You must enter a value');
-    } else if (this.email.hasError('email')) {
-      this.errorMessage.set('Not a valid email');
-    } else {
-      this.errorMessage.set('');
-    }
-  }*/
 
   Validar(){
       if(this.formulario.valid){
