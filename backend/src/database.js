997394ddbbe7a257b24040beaf72b39dfdc9a21f
HailNicole <mconstantem1@est.ups.edu.ec>
2024-07-16 22:25:54 -0500

    arreglado database

--- backend/src/database.js
+++ backend/src/database.js
@@ -7,13 +7,16 @@ const mongoose = require('mongoose');
 
 module.exports = mongoose;*/
 
-const dbUri = 'mongodb+srv://madelin:aAGjJCDZGC24xuVI@cluster0.saarj2n.mongodb.net/impuestos';
+//const dbUri = 'mongodb+srv://madelin:aAGjJCDZGC24xuVI@cluster0.saarj2n.mongodb.net/impuestos';
 
-mongoose.connect(dbUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
+
+const dbUri = 'mongodb+srv://madelin:aAGjJCDZGC24xuVI@cluster0.saarj2n.mongodb.net/impuestos?retryWrites=true&w=majority';
+
+mongoose.connect(dbUri)
+  .then(() => {
     console.log('Conexión a MongoDB Atlas exitosa');
-}).catch((error) => {
+  })
+  .catch((error) => {
     console.error('Error conectándose a MongoDB Atlas:', error);
-});
+  });
+
