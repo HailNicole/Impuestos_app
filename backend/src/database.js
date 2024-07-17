39f085be347fa0d946e7488365afe6eda274add4
HailNicole <mconstantem1@est.ups.edu.ec>
2024-07-16 22:17:23 -0500

    conexion a base modificada

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/angular-auth')
+/*mongoose.connect('mongodb://localhost:27017/angular-auth')
 
 .then(db => console.log('Database is Connected'))
 .catch(err => console.log(err));
 
-module.exports = mongoose;
+module.exports = mongoose;*/
+
+const dbUri = 'mongodb+srv://madelin:aAGjJCDZGC24xuVI@cluster0.saarj2n.mongodb.net/impuestos';
+
+mongoose.connect(dbUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Conexión a MongoDB Atlas exitosa');
+}).catch((error) => {
+    console.error('Error conectándose a MongoDB Atlas:', error);
+});
